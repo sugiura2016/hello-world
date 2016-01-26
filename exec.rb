558a604db6f01a0ee4aae9fca17cdf729691d55d
杉浦貴史 <sugiura8@gmail.com>
2016-01-27 06:36:50 +0900

    Add test code

---
+++ exec.rb
@@ -0,0 +1,18 @@
+class Fizzbuzz
+	def calculate number
+		if number % 3 == 0 && number % 5 == 0
+			puts 'fizzbuzz'
+		elsif number % 3 == 0
+			puts 'fizz'
+		elsif number % 5 == 0
+			puts 'buzz'
+		else
+			puts number
+		end
+	end
+end
+
+obj = Fizzbuzz.new()
+for num in 1..100 do
+	obj.calculate(num)
+end
